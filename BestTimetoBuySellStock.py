085194a0109acb44a43b1a4884d87f55d49db8a2
Amisha Naik <132745732+amishana1k@users.noreply.github.com>
2024-01-06 23:45:14 +0530

    BestTimetoBuySellStock.py

---
+++ BestTimetoBuySellStock.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        i = 0 
+        j = 1
+        max_sum = 0
+        while j < len(prices):
+            if prices[i] >= prices[j] :
+                i = j
+                                
+            else:
+                val = prices[j] - prices[i]
+                max_sum = max(max_sum,val)
+            j += 1
+        return max_sum 
